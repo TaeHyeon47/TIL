c5b1397cbd34c517a6d89fee8e995f2a17da218e
Tae Hyeon Jo <hillskuti@naver.com>
2022-05-15 17:16:16 +0900

    Coding Challenge #2

--- Coding_Challenge/JavaScript_Fundamentals_Part2/script.js
+++ Coding_Challenge/JavaScript_Fundamentals_Part2/script.js
@@ -112,7 +112,6 @@ let totals =
 console.log(`total: ${totals}`);
 
 //////// Other Answer ////////
-
 const calcTip2 = function (bill) {
   return bill >= 50 && bill <= 300 ? bill * 0.15 : bill * 0.2;
 };
