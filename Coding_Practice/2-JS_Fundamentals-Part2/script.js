1c7aaa5ce340ede95a078d51df1baf4052b133c2
Tae Hyeon Jo <hillskuti@naver.com>
2022-05-17 18:17:17 +0900

    Looping Arrays, Breaking and Continuing

--- Coding_Practice/2-JS_Fundamentals-Part2/script.js
+++ Coding_Practice/2-JS_Fundamentals-Part2/script.js
@@ -511,6 +511,55 @@ console.log(age1, age2);
 /////////////////////////////////////
 
 // for loop keeps running while condition is TRUE
-for (let rep = 1; rep <= 10; rep++) {
-  console.log(`Lifting weights repetition ${rep}`);
+// for (let rep = 1; rep <= 10; rep++) {
+//   console.log(`Lifting weights repetition ${rep}`);
+// }
+
+/////////////////////////////////////////////////////
+///// Looping Arrays, Breaking and Continuing ///////
+/////////////////////////////////////////////////////
+
+const jonas = [
+  "Jonas",
+  "Schmedtmann",
+  2037 - 1991,
+  "teacher",
+  ["Michael", "Peter", "Steven"],
+  true,
+];
+
+const types = [];
+
+for (let i = 0; i < jonas.length; i++) {
+  // Reading from jonas array
+  console.log(jonas[i], typeof jonas[i]);
+  // Filling types array
+  // types[i] = typeof jonas[i];
+  types.push(typeof jonas[i]);
+}
+
+console.log(types);
+
+// 빈공간에 배열 넣기
+const years = [1991, 2007, 1969, 2020];
+const ages = [];
+
+for (let i = 0; i < years.length; i++) {
+  ages.push(2037 - years[i]);
+}
+console.log(ages);
+
+// continue and break
+console.log("--- ONLY STRINGS ---");
+// Continue is to exit the current iteration of the loop and continue to the next one.
+// Break is used to completely terminate the whole loop.
+for (let i = 0; i < jonas.length; i++) {
+  if (typeof jonas[i] !== "string") continue;
+  console.log(jonas[i], typeof jonas[i]);
+}
+
+console.log("--- BREAK WITH NUMBER ---");
+for (let i = 0; i < jonas.length; i++) {
+  if (typeof jonas[i] === "number") break;
+  console.log(jonas[i], typeof jonas[i]);
 }
