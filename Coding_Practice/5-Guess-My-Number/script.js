19215a9416dbd4f61d2ee7a9e4e7b70177c3de98
Tae Hyeon Jo <hillskuti@naver.com>
2022-05-18 21:55:03 +0900

    Implementing the Game Logic

--- Coding_Practice/5-Guess-My-Number/script.js
+++ Coding_Practice/5-Guess-My-Number/script.js
@@ -13,6 +13,17 @@ document.querySelector('.guess').value = 23;
 console.log(document.querySelector('.guess').value);
 */
 
+/////////////////////////////////////
+/////// Handling Click Events ///////
+/////////////////////////////////////
+
+// 맞출 숫자는 전역 변수로 1번 선언
+// addEventListener에 넣으면, 클릭할 때마다 새로운 변수가 생성되기 때문에 적합하지 않다.
+const secretNumber = Math.trunc(Math.random() * 20) + 1;
+let score = 20;
+document.querySelector('.number').textContent = secretNumber;
+// document.querySelector('.score').textContent = score;
+
 // An event is something that happens on the page.
 // For example, a mouse click, or a mouse moving, or a key press, or many other events.
 // With an event listener, we can wait for a certain event to happen, and then react to it.
@@ -26,5 +37,23 @@ document.querySelector('.check').addEventListener('click', function () {
   // 0이 falsy value이기 때문에 제외하기 위해 넣어줌..
   if (!guess) {
     document.querySelector('.message').textContent = 'No number!';
+  } else if (guess === secretNumber) {
+    document.querySelector('.message').textContent = 'Correct Number!';
+  } else if (guess > secretNumber) {
+    if (score > 1) {
+      document.querySelector('.message').textContent = 'Too high!';
+      score--;
+      document.querySelector('.score').textContent = score;
+    } else {
+      document.querySelector('.message').textContent = 'You lost the game!';
+    }
+  } else if (guess < secretNumber) {
+    if (score > 1) {
+      document.querySelector('.message').textContent = 'Too Low!';
+      score--;
+      document.querySelector('.score').textContent = score;
+    } else {
+      document.querySelector('.message').textContent = 'You lost the game!';
+    }
   }
 });
