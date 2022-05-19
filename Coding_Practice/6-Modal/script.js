68b51a63c21be31b4e798d9cb07073c2ec961342
Tae Hyeon Jo <hillskuti@naver.com>
2022-05-19 15:33:03 +0900

    Working With Classes

--- Coding_Practice/6-Modal/script.js
+++ Coding_Practice/6-Modal/script.js
@@ -7,6 +7,26 @@ const btnCloseModal = document.querySelector('.close-modal');
 const btnsOpenModal = document.querySelectorAll('.show-modal');
 console.log(btnsOpenModal);
 
+const openModal = function () {
+  console.log('Button clicked');
+  // 'classlist' property itself has a couple of methods. the one we're gonna use now is "remove."
+  // . 은 오직 선택자(selector)에만 사용한다. 이미 선택되어 있기 떄문에 그냥 클래스 이름만 사용한다.
+  modal.classList.remove('hidden');
+  overlay.classList.remove('hidden');
+  //  modal.style.display = 'block';
+};
+
+const closeModal = function () {
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+};
+
+// Each of these three buttons here has their own event handler function attached to it.
 for (let i = 0; i < btnsOpenModal.length; i++) {
-  console.log(btnsOpenModal[i].textContent);
+  btnsOpenModal[i].addEventListener('click', openModal);
 }
+
+// 'closeModal()'은 작동하지 않는다.
+// Because it will immediately called a function, as soon as JavaScript executes this line.
+btnCloseModal.addEventListener('click', closeModal);
+overlay.addEventListener('click', closeModal);
