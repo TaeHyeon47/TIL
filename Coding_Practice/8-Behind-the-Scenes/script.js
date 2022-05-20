b7b507a8eb0669b133611c8a72287045a4de3c79
Tae Hyeon Jo <hillskuti@naver.com>
2022-05-20 18:00:11 +0900

    Scoping in Practice

---
+++ Coding_Practice/8-Behind-the-Scenes/script.js
@@ -0,0 +1,49 @@
+'use strict';
+
+// Every browser has its own JavaScript engine but probably the most well known engine is Google's V-8. but also Node.js
+// So any JavaScript engine always contains a 'call stack' and a 'heap'.
+// call stack : where our code is actually executed using something called execution contexts
+// heap : an unstructured memory pool which stores all the objects that our application needs.
+
+// 이벤트 루프에 대한 설명
+// callback queue : This is a data structure that contains all the callback functions that are ready to be executed.
+// click하면 이벤트가 발생하는 이벤트 리스터가 있다고 가정
+// 클릭 시, 이벤트리스너에 담겨있는 'callback function'은 'callback queue'에 담긴다(put).
+// 이후 call stack이 비어있다면, 'callback function'은 call stack에 담기게 된다.
+// 이러한 과정을 Event loop라고 한다.
+// Basically the event loop takes callback functions from the callback queue
+// and puts them in the call stack so that they can be executed.
+
+// Execution context란
+// Environment in which a piece of JavaScript is executed.
+// It's like a box that stores all the necessary information for some code to be executed.
+// Such as local variables or arguments passed into a function.
+// JavaScript code always runs inside an execution context.
+// in any JavaScript project, no matter how large it is, there is only ever one global execution context.
+// It's always there as the default context, and it's where top-level code will execute.
+
+// Execution context 안에 들어가있는 것들...
+// 1. variable Environment
+//  - let, const and var declarations
+//  - Functions : each function gets its own execution context as soon as the function is called.
+//  - arguments object
+//    : this is a special arguments object. This object contains as the name says all the arguments that were passed
+//    into the function that the current execution context belong to.
+
+// scope is the place in our code where variables are declared.
+
+function calcAge(birthYear) {
+  const age = 2037 - birthYear;
+
+  function printAge() {
+    const output = `You are ${age}, born in ${birthYear}`;
+    console.log(output);
+  }
+  printAge();
+
+  return age;
+}
+
+const firstName = 'Jonas';
+calcAge(1991);
+console.log(age);
