d2501ff218dcfa37af3c9d345f6b6e7d690c8e0f
Tae Hyeon Jo <hillskuti@naver.com>
2022-05-22 18:06:46 +0900

    Git Test

--- Coding_Practice/8-Behind-the-Scenes/script.js
+++ Coding_Practice/8-Behind-the-Scenes/script.js
@@ -313,6 +313,3 @@ jessicaCopy.family.push('John');
 
 console.log('Before marriage: ', jessica2);
 console.log('After marriage: ', jessicaCopy);
-
-//// Test
-console.log('After marriage: ', jessicaCopy);
