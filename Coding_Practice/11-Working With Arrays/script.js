620564638ee34e2122aba0514b93a78b59f7c79a
Tae Hyeon Jo <hillskuti@naver.com>
2022-07-19 12:36:44 +0900

    array method(slice, splice, reverse, concat, join) 연습

--- Coding_Practice/11-Working With Arrays/script.js
+++ Coding_Practice/11-Working With Arrays/script.js
@@ -74,3 +74,55 @@ const currencies = new Map([
 const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
 
 /////////////////////////////////////////////////
+
+//* Array 기본 설명
+//? remember that methods are simply functions that we can call on objects.
+//? So basically, they are functions attached to objects.
+//? So if we have array methods, that means that arrays themselves are also objects.
+//? these array methods are simply functions that are attached to all arrays that we create in JavaScript.
+
+let arr = ['a', 'b', 'c', 'd', 'e'];
+
+// SLICE
+// slice는 객체에 값을 변경하지 않는다.
+
+console.log('SLICE');
+console.log(arr.slice(2));
+//slice 메소드의 start paramter는 값을 포함하지만, end parameter는 값을 포함하지 않는다.
+//최종적으로 index 2, 3만 나타나게 된다.
+console.log(arr.slice(2, 4));
+console.log(arr.slice(-2));
+console.log(arr.slice(-1));
+console.log(arr.slice(1, -2));
+console.log(arr.slice());
+console.log([...arr]);
+
+// SPLICE
+// splice는 객체에 값을 변경한다.
+console.log('SPLICE');
+// console.log(arr.splice(2));
+// splice로 요소가 추출되어 origianl array 값이 변경되어 있다.
+arr.splice(-1);
+console.log(arr);
+arr.splice(1, 2);
+console.log(arr);
+
+// REVERSE
+// reverse는 객체에 값을 변경한다.
+console.log('REVERSE');
+arr = ['a', 'b', 'c', 'd', 'e'];
+const arr2 = ['j', 'i', 'h', 'g', 'f'];
+console.log(arr2.reverse()); // ['f', 'g', 'h', 'i', 'j']
+console.log(arr2); // ['f', 'g', 'h', 'i', 'j']
+//? 어떤 메소드가 array의 값을 변경하는지 알고 있는 것은 중요하다.
+
+// CONCAT
+// concat은 객체에 값을 변경하지 않는다.
+console.log('CONCAT');
+const letters = arr.concat(arr2);
+console.log(letters);
+console.log([...arr, ...arr2]);
+
+// JOIN
+console.log('JOIN');
+console.log(letters.join(' - ')); // string 값으로 ' - ' 값이 입력되어 나타난다.
