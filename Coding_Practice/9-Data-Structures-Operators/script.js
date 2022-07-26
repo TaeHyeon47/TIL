895a07fa8019b1072671be5ad2178587412dcd55
Tae Hyeon Jo <hillskuti@naver.com>
2022-07-26 23:01:04 +0900

    Rest Pattern and Parameters

--- Coding_Practice/9-Data-Structures-Operators/script.js
+++ Coding_Practice/9-Data-Structures-Operators/script.js
@@ -45,7 +45,57 @@ const restaurant = {
   orderPasta: function (ing1, ing2, ing3) {
     console.log(`Here is your declicius pasta with ${ing1}, ${ing2}, ${ing3}`);
   },
+
+  orderPizza: function (mainIngredient, ...otherIngredients) {
+    console.log(mainIngredient);
+    console.log(otherIngredients);
+  },
 };
+
+//? Rest Pattern and Parameters
+// The rest pattern looks exactly like the spread operator So it has the same syntax with the three dots
+// but it actually does the opposite of the spread operator.
+//* The spread operator is to unpack an array, while rest is to pack elements into an array
+
+// 1) Destructuring
+
+// Spread, because on RIGHT side of =
+const arr = [1, 2, ...[3, 4]];
+
+// REST, because on LEFT side of =
+const [a, b, ...others] = [1, 2, 3, 4, 5];
+console.log(a, b, others);
+
+// The rest syntax collects all the array after the last variable.
+// So in this case here 'risotto'. It does not include any skipped elements like 'Pasta'.
+// Rest 문법은 마지막 요소를 기준으로 나머지를 모두 가져오는 것이기에 오직 1개만 사용할 수 있다.
+const [pizza, , risotto, ...otherFood] = [
+  ...restaurant.mainMenu,
+  ...restaurant.starterMenu,
+];
+console.log(pizza, risotto, otherFood);
+
+//Objects
+const { sat, ...weekdays } = restaurant.openingHours;
+console.log(weekdays);
+
+// 2) Functions
+const add = function (...numbers) {
+  let sum = 0;
+  for (let i = 0; i < numbers.length; i++) sum += numbers[i];
+  console.log(sum);
+};
+add(2, 3);
+add(5, 3, 7, 2);
+add(8, 2, 5, 3, 2, 1, 4);
+
+const x = [23, 5, 7];
+add(...x); // Spread operator로 풀어주고 넣어준다.
+
+restaurant.orderPizza('mushrooms', 'onion', 'olives', 'spinach');
+restaurant.orderPizza('mushrooms');
+
+/*
 
 //? The Spread Operator (...)
 // what the spread operator does is to basically take all the values out of this 'arr' array.
@@ -113,6 +163,8 @@ const restaurantCopy = { ...restaurant };
 restaurantCopy.name = 'Ristorante Roma';
 console.log(restaurantCopy.name);
 console.log(restaurant.name);
+
+*/
 
 /*
 
