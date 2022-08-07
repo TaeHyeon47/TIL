4d92c58523d656dae915c3eb88bb721a5022baf0
Tae Hyeon Jo <hillskuti@naver.com>
2022-08-07 22:50:29 +0900

    코딩 챌린지 1번문제

--- Coding_Practice/9-Data-Structures-Operators/script.js
+++ Coding_Practice/9-Data-Structures-Operators/script.js
@@ -20,7 +20,61 @@ BONUS: Create an object called 'scorers' which contains the names of the players
         Lewandowski: 2
       }
 
+
 GOOD LUCK 😀
+*/
+
+const game = {
+  team1: 'Bayern Munich',
+  team2: 'Borrussia Dortmund',
+  players: [
+    [
+      'Neuer',
+      'Pavard',
+      'Martinez',
+      'Alaba',
+      'Davies',
+      'Kimmich',
+      'Goretzka',
+      'Coman',
+      'Muller',
+      'Gnarby',
+      'Lewandowski',
+    ],
+    [
+      'Burki',
+      'Schulz',
+      'Hummels',
+      'Akanji',
+      'Hakimi',
+      'Weigl',
+      'Witsel',
+      'Hazard',
+      'Brandt',
+      'Sancho',
+      'Gotze',
+    ],
+  ],
+  score: '4:0',
+  scored: ['Lewandowski', 'Gnarby', 'Lewandowski', 'Hummels'],
+  date: 'Nov 9th, 2037',
+  odds: {
+    team1: 11.33,
+    x: 3.25,
+    team2: 6.5,
+  },
+};
+
+// 1. Loop over the game.scored array and print each player name to the console, along with the goal number (Example: "Goal 1: Lewandowski")
+
+// 나의 답변 ㅠㅠ
+// for (const scored of game.scored) {
+//   console.log(`Goal  : ${scored}`);
+// }
+
+for (const [i, player] of game.scored.entries()) {
+  console.log(`Goal ${i + 1}" ${player}`);
+}
 
 /////////////////////////////////
 //? Coding Challenge #1
