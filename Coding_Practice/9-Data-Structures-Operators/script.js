636d486bcbfbb018730a28ceb5414fcb6fbdcd38
Tae Hyeon Jo <hillskuti@naver.com>
2022-09-01 22:10:11 +0900

    코딩챌린지 나의 답

--- Coding_Practice/9-Data-Structures-Operators/script.js
+++ Coding_Practice/9-Data-Structures-Operators/script.js
@@ -284,6 +284,64 @@ const restaurant = {
   },
 };
 
+//? Coding Challenge #3
+
+/*
+Let's continue with our football betting app! This time, we have a map called
+'gameEvents' (see below) with a log of the events that happened during the
+game. The values are the events themselves, and the keys are the minutes in which
+each event happened (a football game has 90 minutes plus some extra time).
+Your tasks:
+1. Create an array 'events' of the different game events that happened (no
+duplicates)
+2. After the game has finished, is was found that the yellow card from minute 64
+was unfair. So remove this event from the game events log.
+3. Compute and log the following string to the console: "An event happened, on
+average, every 9 minutes" (keep in mind that a game has 90 minutes)
+4. Loop over 'gameEvents' and log each element to the console, marking
+whether it's in the first half or second half (after 45 min) of the game, like this:
+[FIRST HALF] 17: ⚽ GOAL
+GOOD LUCK �
+*/
+
+const gameEvents = new Map([
+  [17, '⚽ GOAL'],
+  [36, '� Substitution'],
+  [47, '⚽ GOAL'],
+  [61, '� Substitution'],
+  [64, '� Yellow card'],
+  [69, '� Red card'],
+  [70, '� Substitution'],
+  [72, '� Substitution'],
+  [76, '⚽ GOAL'],
+  [80, '⚽ GOAL'],
+  [92, '� Yellow card'],
+]);
+
+// 1.
+const events = [...gameEvents];
+console.log(events);
+
+// 2.
+gameEvents.delete(64);
+console.log(gameEvents);
+
+// 3.
+const gameEventskeys = 90 / [...gameEvents.keys()].length;
+console.log(`An event happend, on average, every ${gameEventskeys} minutes`);
+
+// 4.
+
+for (const [key, value] of gameEvents) {
+  if (key <= 45) {
+    console.log(`[FIRST HALF]${key}: ${value}`);
+  } else {
+    console.log(`[SECOND HALF]${key}: ${value}`);
+  }
+}
+
+/*
+
 //? Maps: Iteration
 
 // Map을 set으로 세팅하기에는 무겁다. 그래서 아래의 방식으로 사용한다.
@@ -320,6 +378,8 @@ console.log(question.keys());
 console.log(question.values());
 // MapIterator를 Object로 변경할 수 있다.
 console.log([...question.values()]);
+
+*/
 
 /*
 
