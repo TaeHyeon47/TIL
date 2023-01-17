da0a5884db3ec7b202ad0c7f915f9b3840ef343d
Tae Hyeon Jo <hillskuti@naver.com>
2023-01-17 22:18:05 +0900

    프로미스 추가학습

--- Coding_Practice/16-Asynchronous/script.js
+++ Coding_Practice/16-Asynchronous/script.js
@@ -1,7 +1,7 @@
-"use strict";
+'use strict';
 
-const btn = document.querySelector(".btn-country");
-const countriesContainer = document.querySelector(".countries");
+const btn = document.querySelector('.btn-country');
+const countriesContainer = document.querySelector('.countries');
 
 // const renderCountry = function (data, className = "") {
 //   const html = `<article class="country ${className}">
@@ -78,70 +78,71 @@ const countriesContainer = document.querySelector(".countries");
 ////// Welcome to Callback Hell //////
 //////////////////////////////////////
 
-const renderCountry = function (data, className = "") {
-  const html = `<article class="country ${className}">
-   <img class="country__img" src="${data.flag}" />
-   <div class="country__data">
-     <h3 class="country__name">${data.name}</h3>
-     <h4 class="country__region">${data.region}</h4>
-     <p class="country__row"><span>👫</span>${(
-       +data.population / 1000000
-     ).toFixed(1)}</p>
-     <p class="country__row"><span>🗣️</span>${data.languages[0].name}</p>
-     <p class="country__row"><span>💰</span>${data.currencies[0].name}</p>
-   </div>
- </article>`;
-  countriesContainer.insertAdjacentHTML("beforeend", html);
-  countriesContainer.style.opacity = 1;
-};
+// const renderCountry = function (data, className = '') {
+//   const html = `<article class="country ${className}">
+//    <img class="country__img" src="${data.flag}" />
+//    <div class="country__data">
+//      <h3 class="country__name">${data.name}</h3>
+//      <h4 class="country__region">${data.region}</h4>
+//      <p class="country__row"><span>👫</span>${(
+//        +data.population / 1000000
+//      ).toFixed(1)}</p>
+//      <p class="country__row"><span>🗣️</span>${data.languages[0].name}</p>
+//      <p class="country__row"><span>💰</span>${data.currencies[0].name}</p>
+//    </div>
+//  </article>`;
+//   countriesContainer.insertAdjacentHTML('beforeend', html);
+//   countriesContainer.style.opacity = 1;
+// };
 
-const getCountryAndNeighbour = function (country) {
-  // AJAX call country 1
-  const request = new XMLHttpRequest();
-  request.open("GET", `https://restcountries.com/v2/name/${country}`);
-  request.send();
+// const getCountryAndNeighbour = function (country) {
+//   // AJAX call country 1
+//   const request = new XMLHttpRequest();
+//   request.open('GET', `https://restcountries.com/v2/name/${country}`);
+//   request.send();
 
-  request.addEventListener("load", function () {
-    const [data] = JSON.parse(this.responseText);
-    console.log(data);
+//   request.addEventListener('load', function () {
+//     const [data] = JSON.parse(this.responseText);
+//     console.log(data);
 
-    // Render country 1
-    renderCountry(data);
-    //Get neighbour country (2)
-    const [neighbour] = data.borders;
-    console.log(neighbour);
+//     // Render country 1
+//     renderCountry(data);
+//     //Get neighbour country (2)
+//     const [neighbour] = data.borders;
+//     console.log(neighbour);
 
-    if (!neighbour) return;
+//     if (!neighbour) return;
 
-    // AJAX call country 2
-    const request2 = new XMLHttpRequest();
-    request2.open("GET", `https://restcountries.com/v2/alpha/${neighbour}`);
-    request2.send();
+//     // AJAX call country 2
+//     const request2 = new XMLHttpRequest();
+//     request2.open('GET', `https://restcountries.com/v2/alpha/${neighbour}`);
+//     request2.send();
 
-    request2.addEventListener("load", function () {
-      const data2 = JSON.parse(this.responseText);
-      console.log(data2);
-      renderCountry(data2, "neighbour");
-    });
-  });
-};
+//     //? 콜백 함수 안에 또 콜백이 들어간다.
+//     request2.addEventListener('load', function () {
+//       const data2 = JSON.parse(this.responseText);
+//       console.log(data2);
+//       renderCountry(data2, 'neighbour');
+//     });
+//   });
+// };
 
-getCountryAndNeighbour("usa");
+// getCountryAndNeighbour('usa');
 
 // // callback hell is when we have a lot of nested callbacks
 // // in order to execute asynchronous tasks in sequence.
-setTimeout(() => {
-  console.log(`1 second passed`);
-  setTimeout(() => {
-    console.log(`2 second passed`);
-    setTimeout(() => {
-      console.log(`3 second passed`);
-      setTimeout(() => {
-        console.log(`4 second passed`);
-      }, 1000);
-    }, 1000);
-  }, 1000);
-}, 1000);
+// setTimeout(() => {
+//   console.log(`1 second passed`);
+//   setTimeout(() => {
+//     console.log(`2 second passed`);
+//     setTimeout(() => {
+//       console.log(`3 second passed`);
+//       setTimeout(() => {
+//         console.log(`4 second passed`);
+//       }, 1000);
+//     }, 1000);
+//   }, 1000);
+// }, 1000);
 
 // Since ES6 there is actually a way of escaping callback hell
 // by using something called promises.
